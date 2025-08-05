563a0a0b8469194b7000368e8d8f2af83b808a98
nicolasdumontnid <nicolas.dumont@telemis.com>
2025-08-05 10:04:40 +0200

    Remove incomplete patients component file

--- src/app/components/patients/patients.component.ts
+++
@@ -1,5 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Patient, AnatomicalRegion } from '../../models/patient.model';
-import { PatientService } from '../../services/patient.service';
